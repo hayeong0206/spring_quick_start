c60a2b3d4595599cce8bc78d391c10ff15bcae8c
lee <lee@172.20.10.4>
2023-10-12 21:42:03 +0900

    4.1.2 의존성 관계 문제점 1. SonySpeaker 객체가 개 생성됨 2. 스피커를 변경하고자 할때 모든 메소드를 수정해야 함

--- BoardWeb/src/main/java/polymorphism/SamsungTV.java
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -1,17 +1,10 @@
 package polymorphism;
 
 public class SamsungTV implements TV {
-	public void initMethod() {
-		System.out.println("객체 초기화 작업 처리..");
-	}
-	//멤버변수에 대한 초기화 작업 처리
-	public void destroyMethod() {
-		System.out.println("객체 삭제 전에 처리할 로직 처리..");
-	}
-	//객체를 삭제하기 직전에 호출될 임읭의 메소드 지정
+	private SonySpeaker speaker;
+	
 	public SamsungTV() {
 		System.out.println("===> SamsungTV 객체 생성");
-		//객체가 언제 생성되는지 알기위함 
 	}
 	public void powerOn() {
 		System.out.println("SamsungTV --- 전원 켠다.");
@@ -20,9 +13,12 @@ public class SamsungTV implements TV {
 		System.out.println("SamsungTV --- 전원 끈다.");
 	}
 	public void volumeUp() {
-		System.out.println("SamsungTV --- 소리 올린다.");
+		speaker = new SonySpeaker();
+		speaker.volumeUp();
 	}
 	public void volumeDown() {
-		System.out.println("SamsungTV --- 소리 내린다.");
+		speaker = new SonySpeaker();
+		speaker.volumeUp();
 	}
+	//samsungTV 클래스의 볼륨 조절 기능을 SonySpeaker가 이용하도록 수정
 }
---
+++ BoardWeb/src/main/java/polymorphism/SonySpeaker.java
@@ -0,0 +1,14 @@
+package polymorphism;
+
+public class SonySpeaker {
+	public SonySpeaker() {
+		System.out.println("===> SonnySpeaker 객체 생성");
+	}
+	
+	public void volumeUp() {
+		System.out.println("SonnySpeaker---소리 올린다.");
+	}
+	public void volumeDown() {
+		System.out.println("SonnySpeaker---소리 내린다.");
+	}
+}
