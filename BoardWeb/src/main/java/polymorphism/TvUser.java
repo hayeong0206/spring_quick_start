81b299b9bf3c5a8cd5292ef55b3f2d550ed4db37
lee <lee@172.20.10.4>
2023-10-12 21:11:26 +0900

    3.2.3 <bean> 엘리먼트 class속성만 사용해도 객체생성은 상관 없음 하지만 객체를 요청하려면 이름이 반드시 지정되어야함.

--- BoardWeb/src/main/java/polymorphism/TvUser.java
+++ BoardWeb/src/main/java/polymorphism/TvUser.java
@@ -10,14 +10,6 @@ public class TvUser {
 		AbstractApplicationContext factory =
 				new GenericXmlApplicationContext("applicationContext.xml");
 		
-		//2. Spring 컨테이너로부터 필요한 객체를 요청(Look up)한다.
-		TV tv = (TV)factory.getBean("tv");
-		tv.powerOn();
-		tv.volumeUp();
-		tv.volumeDown();
-		tv.powerOff();
-		
-		//3. Spring 컨테이너를 종료한다.
-		factory.close();
+
 	}
 }
