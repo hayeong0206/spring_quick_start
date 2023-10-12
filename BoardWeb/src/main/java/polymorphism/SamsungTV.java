edd83c36e66de5437052352ab262e50549e6455d
lee <lee@172.20.10.4>
2023-10-12 21:20:48 +0900

    3.2.4 엘리멘트 속성
    [실행결과]
    DEBUG: org.springframework.beans.factory.xml.XmlBeanDefinitionReader -
    Loaded 1 bean definitions from class path resource
    [applicationContext.xml]
    DEBUG: org.springframework.context.support.GenericXmlApplicationContext
    - Refreshing
    org.springframework.context.support.GenericXmlApplicationContext@4b5189ac
    DEBUG: org.springframework.beans.factory.support.DefaultListableBeanFactory
    - Creating shared instance of singleton bean 'tv'
    ===> SamsungTV 객체 생성
    객체 초기화 작업 처리..
    SamsungTV --- 전원 켠다.
    SamsungTV --- 소리 올린다.
    SamsungTV --- 소리 내린다.
    SamsungTV --- 전원 끈다.
    DEBUG: org.springframework.context.support.GenericXmlApplicationContext
    - Closing org.springframework.context.support.GenericXmlApplicationContext@4b5189ac,
    started on Thu Oct 12 21:19:03 KST 2023
    객체 삭제 전에 처리할 로직 처리..
    
    [설명]
    initMethod는 객체생성 직후 호출
    destroyMethod는 객체 삭제되기 직전 호

--- BoardWeb/src/main/java/polymorphism/SamsungTV.java
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -1,6 +1,14 @@
 package polymorphism;
 
 public class SamsungTV implements TV {
+	public void initMethod() {
+		System.out.println("객체 초기화 작업 처리..");
+	}
+	//멤버변수에 대한 초기화 작업 처리
+	public void destroyMethod() {
+		System.out.println("객체 삭제 전에 처리할 로직 처리..");
+	}
+	//객체를 삭제하기 직전에 호출될 임읭의 메소드 지정
 	public SamsungTV() {
 		System.out.println("===> SamsungTV 객체 생성");
 		//객체가 언제 생성되는지 알기위함 
