b38dc18fe365ba8496fabbf0601262b74ca882c6
lee <lee@localhost>
2023-10-12 19:55:38 +0900

    class 02 최상위 부모로 사용한 TV인터페이스 추가, 모든 TV가 가져야할 공통 메소드를 추상메소드로 선언 -> 두개의 티비 클래스가 모두 같은 추상메소드로 정의되어야 함
    => 티비를 변경할 때 객체만 변경하면 되므로 유지보수가 더욱 편해짐

--- BoardWeb/src/main/java/polymorphism/TvUser.java
+++ BoardWeb/src/main/java/polymorphism/TvUser.java
@@ -2,8 +2,8 @@ package polymorphism;
 
 public class TvUser {
 	public static void main(String[] args) {
-		//SamsungTV tv = new SamsungTV();
-		LgTV tv = new LgTV();
+		SamsungTV tv = new SamsungTV();
+		//LgTV tv = new LgTV();
 		tv.powerOn();
 		tv.powerOff();
 		tv.volumeUp();
