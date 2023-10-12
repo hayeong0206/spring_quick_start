2766d4c3c6546b3a73738a6d8e82ae8c86334235
lee <lee@172.20.10.4>
2023-10-12 20:23:23 +0900

    class 2.3.3 디자인 패턴 이용하기

--- BoardWeb/src/main/java/polymorphism/TvUser.java
+++ BoardWeb/src/main/java/polymorphism/TvUser.java
@@ -2,8 +2,9 @@ package polymorphism;
 
 public class TvUser {
 	public static void main(String[] args) {
-		SamsungTV tv = new SamsungTV();
-		//LgTV tv = new LgTV();
+		BeanFactory factory = new BeanFactory();
+		TV tv = (TV)factory.getBean(args[0]);
+		//사용할 TV 객체 획득 arguments에 매개변수 입력 후 실
 		tv.powerOn();
 		tv.powerOff();
 		tv.volumeUp();
