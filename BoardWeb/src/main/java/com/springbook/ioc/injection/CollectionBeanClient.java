49833228a8b95830238dcbdbcfd15ccb81561307
lee <lee@172.20.10.4>
2023-10-14 05:41:31 +0900

    4.4.2 Set타입 매핑
    *책 오류 (CollectionBeanClient.java)
    CollectionBean.java import 지우기
    List<String> -> Set<String>

--- BoardWeb/src/main/java/com/springbook/ioc/injection/CollectionBeanClient.java
+++ BoardWeb/src/main/java/com/springbook/ioc/injection/CollectionBeanClient.java
@@ -1,10 +1,9 @@
 package com.springbook.ioc.injection;
 
-import java.util.List;
+import java.util.Set;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
-import com.springbook.ioc.injection.CollectionBean;
 
 public class CollectionBeanClient {
 	public static void main(String[] args) {
@@ -12,7 +11,7 @@ public class CollectionBeanClient {
 				new GenericXmlApplicationContext("applicationContext.xml");
 		
 		CollectionBean bean = (CollectionBean) factory.getBean("collectionBean");
-		List<String> addressList = bean.getAddressList();
+		Set<String> addressList = bean.getAddressList();
 		for (String address : addressList) {
 			System.out.println(address.toString());
 		}
