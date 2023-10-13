c8c8377d48179fa47e65e684492e5a223b9bee08
lee <lee@172.20.10.4>
2023-10-14 04:38:22 +0900

    4.2.2 의존관계 변경 최상위 인터페이스 추가 TV나 Speaker 변경 과정에서 자바 코드의 변경이 필요 없음

--- BoardWeb/src/main/java/polymorphism/SamsungTV.java
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -1,19 +1,25 @@
 package polymorphism;
 
 public class SamsungTV implements TV {
-	private SonySpeaker speaker;
+	private Speaker speaker;
+	private int price;
 	
 	public SamsungTV() {
 		System.out.println("===> SamsungTV 객체 생성");
 	}
-	//생성자 인젝션 이용
-	public SamsungTV(SonySpeaker speaker) {
+	
+	public SamsungTV(Speaker speaker ) {
 		System.out.println("===> SamsungTV(2) 객체생성");
 		this.speaker = speaker;
 	}
-	//SamsungTV클래스에 생성자 추가
+
+	public SamsungTV(Speaker speaker, int price) {
+		System.out.println("===> SamsungTV(3) 객체 생성");
+		this.speaker = speaker;
+		this.price = price;
+	}
 	public void powerOn() {
-		System.out.println("SamsungTV --- 전원 켠다.");
+		System.out.println("SamsungTV --- 전원 켠다. (가격: " + price + ")");
 	}
 	public void powerOff() {
 		System.out.println("SamsungTV --- 전원 끈다.");
