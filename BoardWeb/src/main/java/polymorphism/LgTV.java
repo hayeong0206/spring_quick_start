3fa088a7f65af61d1825d2aaf010690c46be8a9a
lee <lee@172.20.10.4>
2023-10-14 06:19:37 +0900

    5.1.3 @Component - 어노테이션 설

--- BoardWeb/src/main/java/polymorphism/LgTV.java
+++ BoardWeb/src/main/java/polymorphism/LgTV.java
@@ -1,6 +1,15 @@
 package polymorphism;
 
+import org.springframework.stereotype.Component;
+
+@Component("tv")
+//클라이언트 요청을 위해 아이디설정
 public class LgTV implements TV {
+	
+	public LgTV() {
+		System.out.println("===> LgTV 객체 생성");
+	}
+	
 	public void powerOn() {
 		System.out.println("LgTV --- 전원 켠다.");
 	}
