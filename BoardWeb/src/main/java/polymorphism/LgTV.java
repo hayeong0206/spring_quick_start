312b86ddbfad8a80d2e0e38422fc8e11b6795118
lee <lee@172.20.10.4>
2023-10-15 00:36:36 +0900

    5.2.5 어노테이션과 XML 설정 병행하여 사용하기
    XML
    [장점] 자바 소스를 수정하지 않고 파일의 설정만 변경하면 되어서 유지보수가 편함
    [단점] XML설정에 대한 부담감 + 해석해야 의존성 파악 가능
    
    어노테이션
    [장점] XML 설정에 대한 부담감 없음 + 의존관계에 대한 정보가 코드에 있음
    [단점] 자바 소스를 교체하지 않으면 speaker를 교체할 수 없

--- BoardWeb/src/main/java/polymorphism/LgTV.java
+++ BoardWeb/src/main/java/polymorphism/LgTV.java
@@ -1,12 +1,12 @@
 package polymorphism;
 
-import javax.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("tv")
 public class LgTV implements TV {
 	
-	@Resource(name="apple")
+	@Autowired
 	private Speaker speaker;
 	
 	public LgTV() {
