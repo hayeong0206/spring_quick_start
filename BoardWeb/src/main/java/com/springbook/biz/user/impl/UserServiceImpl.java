3545cc145adca5053e64aef9e82748c597b3e10e
lee <lee@218.38.137.28>
2023-10-16 06:56:32 +0900

    7.7 어노테이션 적용

--- BoardWeb/src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
+++ BoardWeb/src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.springbook.biz.user.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.springbook.biz.user.UserService;
 import com.springbook.biz.user.UserVO;
 
+@Service("userService")
 public class UserServiceImpl implements UserService{
+	@Autowired
 	private UserDAO userDAO;
 	//멤버 변수로 UserDAO 객체 선언 -> DB 연동 처리
 	
