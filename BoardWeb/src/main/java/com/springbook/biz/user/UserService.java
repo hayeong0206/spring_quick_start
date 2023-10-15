e72ac7a6c9405fe261c27d9e9b64fd8849839820
lee <lee@218.38.137.28>
2023-10-16 06:44:44 +0900

    git branch  바꿨다가 클래스 실행이 안됨..

--- BoardWeb/src/main/java/com/springbook/biz/user/UserService.java
+++ BoardWeb/src/main/java/com/springbook/biz/user/UserService.java
@@ -2,4 +2,8 @@ package com.springbook.biz.user;
 
 public interface UserService {
 
+	//CRUD 기능의 메소드 구현
+	//회원 등록
+	UserVO getUser(UserVO vo);
+
 }
--- BoardWeb/src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
+++ BoardWeb/src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
@@ -1,5 +1,17 @@
 package com.springbook.biz.user.impl;
 
-public class UserServiceImpl {
+import com.springbook.biz.user.UserService;
+import com.springbook.biz.user.UserVO;
 
+public class UserServiceImpl implements UserService{
+	private UserDAO userDAO;
+	//멤버 변수로 UserDAO 객체 선언 -> DB 연동 처리
+	
+	public void setUserDAO(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+	
+	public UserVO getUser(UserVO vo) {
+		return userDAO.getUser(vo);
+	}
 }
