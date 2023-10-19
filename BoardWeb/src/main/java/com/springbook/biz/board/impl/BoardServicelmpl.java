5bbc49f48d334b3d945f3ce195e1ebf7b8da5854
lee <lee@localhost>
2023-10-19 10:26:34 +0900

    1.1 AOP 이해하기
    BoardServiceImpl 클래스와 LogAdvice 객체가 소스코드에서 강하게 결합되어 있어서 각종 상황에 유연하게 대처할
    수 없음

--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServicelmpl.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServicelmpl.java
@@ -7,30 +7,41 @@ import org.springframework.stereotype.Service;
 
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.common.LogAdvice;
 
 @Service("boardService")
 public class BoardServicelmpl implements BoardService {
 	@Autowired
 	private BoardDAO boardDAO;
+	private LogAdvice log;
+	
+	public BoardServicelmpl() {
+		log = new LogAdvice();
+	}
 
 	public void insertBoard(BoardVO vo) {
+		log.printLog();
 		boardDAO.insertBoard(vo);
 	}
 
 	public void updateBoard(BoardVO vo) {
+		log.printLog();
 		boardDAO.updateBoard(vo);
 		
 	}
 
 	public void deleteBoard(BoardVO vo) {
+		log.printLog();
 		boardDAO.deleteBoard(vo);
 	}
 
 	public BoardVO getBoard(BoardVO vo) {
+		log.printLog();
 		return boardDAO.getBoard(vo);
 	}
 
 	public List<BoardVO> getBoardList(BoardVO vo) {
+		log.printLog();
 		return boardDAO.getBoardList(vo);
 	}
 	
---
+++ BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -0,0 +1,9 @@
+package com.springbook.biz.common;
+
+//모든 비즈니스 메소드가 실행되기 직전에 공통으로 처리할 로직
+
+public class LogAdvice {
+	public void printLog() {
+		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
+	}
+}
