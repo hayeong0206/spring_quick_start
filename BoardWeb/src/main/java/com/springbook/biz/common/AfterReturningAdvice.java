863956a18ab197d5bea0ad00dd7ede4458e59e81
lee <lee@localhost>
2023-10-23 05:31:31 +0900

    4.3 After Returning 어드바이스 비즈니스가 수행되고 나서, 결과 데이터를 리턴할 때 동작 어떤 메소드가 어떤 값을 리턴했는지 알아야 사후 처리 기능을 다양하게 구현할 수 있음.

--- BoardWeb/src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
@@ -1,7 +1,21 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
+import com.springbook.biz.user.UserVO;
+
 public class AfterReturningAdvice {
-	public void afterLog() {
-		System.out.println("[사후 처리] 비즈니스 로직 수행 후 동작");
+	public void afterLog(JoinPoint jp, Object returnObj) {
+		//클라이언트가 호출한 비즈니스 메소드 정보를 알아내기 위해서 JoinnPoint 객체를 첫 번째 매개변수로 선언
+		//바인드변수(Object type) - 비즈니스 메소드가 리턴한 결괏값을 바인딩할 목적으로 사용, 어떤 값이 리턴될지 모르니 object타입으로 선
+		String method = jp.getSignature().getName();
+		
+		if(returnObj instanceof UserVO) {
+			UserVO user = (UserVO) returnObj;
+			if(user.getRole().equals("Admin")) {
+				System.out.println(user.getName()+" 로그인(Admin)");
+			}
+		}
+		System.out.println("[사후 처리] " + method + "() 메소드 리턴값 : " + returnObj.toString());
 	}
 }
