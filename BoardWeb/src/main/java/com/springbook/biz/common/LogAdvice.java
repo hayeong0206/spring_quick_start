9f34f3d41a9ee7478fec700f0d274d63d3b6b8b5
lee <lee@localhost>
2023-10-27 11:00:07 +0900

    5.1.3 어드바이스 설정
    횡단 곤심에 해당하는 어드바이스 메소드가 구현되어 있음

--- BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -2,6 +2,7 @@ package com.springbook.biz.common;
 
 import org.springframework.stereotype.Service;
 
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 @Service
@@ -9,6 +10,11 @@ public class LogAdvice {
 	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
 	public void allPointcut() {}
 	
-	@Pointcut("execution(* com.spirngbook.biz..*Impl.get*(..))")
-	public void getPointcut() {}
+	@Before("allPointcut()")
+	public void printlog() {
+		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
+	}
+	
+	//allPointcut() 참조 메소드로 지정한 비즈니스 메소드가 호출될 때,
+	//어드바이스 메소드인 printLog() 메소드가 Before 형태로 동작하도록 설정
 }
