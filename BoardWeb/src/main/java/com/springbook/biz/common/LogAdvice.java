6e117d4c73b5b29510d30c72abdd7e3b8cd14822
lee <lee@localhost>
2023-10-27 10:55:57 +0900

    5.1.2 포인트컷 설정
    애스팩트 설정에서 특정 포인트컷을 참조할 수 있음
    참조메소드는 메소드 몸체가 비어있는, 구현 로직이 없는 메소드

--- BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,10 +1,14 @@
 package com.springbook.biz.common;
 
-import org.aspectj.lang.JoinPoint;
-//모든 비즈니스 메소드가 실행되기 직전에 공통으로 처리할 로직
+import org.springframework.stereotype.Service;
 
+import org.aspectj.lang.annotation.Pointcut;
+
+@Service
 public class LogAdvice {
-	public void printLog(JoinPoint jp) {
-		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
-	}
+	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
+	public void allPointcut() {}
+	
+	@Pointcut("execution(* com.spirngbook.biz..*Impl.get*(..))")
+	public void getPointcut() {}
 }
