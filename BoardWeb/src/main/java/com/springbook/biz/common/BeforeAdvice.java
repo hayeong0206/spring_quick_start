037ce6f2de12aece71f84c926163d840cd93d197
lee <lee@localhost>
2023-10-27 11:51:53 +0900

    5.2.6 외부 포인트컷 찹조하기
    비슷하거나 같은 포인트컷이 반복 선언되는 문제가 발생
    -> 포인트컷을 외부에 독립된 클래스에 따로 설정하도록 함.

--- BoardWeb/src/main/java/com/springbook/biz/common/BeforeAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/BeforeAdvice.java
@@ -3,16 +3,12 @@ package com.springbook.biz.common;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Service;
 
 @Service
 @Aspect
 public class BeforeAdvice {
-	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
-	public void allPointcut() {}
-	
-	@Before("allPointcut()")
+	@Before("PointcutCommon.allPointcut()")
 	public void beforeLog(JoinPoint jp) {
 		String method = jp.getSignature().getName();
 		//getSignature - 클라이언트가 호출한 메소드 이름
