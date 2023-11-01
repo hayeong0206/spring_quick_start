8e03c2f3a9e15c433e5f8dbe748d0d2832ec16c8
lee <lee@localhost>
2023-11-01 15:43:06 +0900

    3.3 로그인 기능 구현하기

--- BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.springbook.biz.user.UserVO;
+import com.springbook.biz.user.impl.UserDAO;
+
 
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -30,6 +33,22 @@ public class DispatcherServlet extends HttpServlet {
 		//2. 클라이언트의 요청 path에 따라 적절히 분기처리 한다.
 		if(path.equals("/login.do")) {
 			System.out.println("로그인 처리");
+			
+			String id = request.getParameter("id");
+			String password = request.getParameter("password");
+			
+			UserVO vo = new UserVO();
+			vo.setId(id);
+			vo.setPassword(password);
+			
+			UserDAO userDAO = new UserDAO();
+			UserVO user = userDAO.getUser(vo);
+			
+			if(user != null) {
+				response.sendRedirect("getBoardList.jsp");
+			} else {
+				response.sendRedirect("login.jsp");
+			}
 		} else if(path.equals("/logout.do")) {
 			System.out.println("로그아웃 처리");
 		} else if(path.equals("/insertBoard.do")) {
