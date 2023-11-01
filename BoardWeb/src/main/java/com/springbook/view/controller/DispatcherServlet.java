4eebf9698ed28a0122663593559d679f8c73258b
lee <lee@localhost>
2023-11-01 19:43:17 +0900

    3.7 글 수정 기능 구현하기

--- BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -76,8 +76,28 @@ public class DispatcherServlet extends HttpServlet {
 			
 			//3. 화면 네비게이션 
 			response.sendRedirect("getBoardList.do");
+			
 		} else if(path.equals("/updateBoard.do")) {
 			System.out.println("글 수정 처리");
+			
+			//1. 사용자 입력 정보 추출
+			request.setCharacterEncoding("UTF-8");
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			String seq = request.getParameter("seq");
+			
+			//2. DB 연동 처리
+			BoardVO vo = new BoardVO();
+			vo.setTitle(title);
+			vo.setContent(content);
+			vo.setSeq(Integer.parseInt(seq));
+			
+			BoardDAO boardDAO = new BoardDAO();
+			boardDAO.updateBoard(vo);
+			
+			//3.화면 네비게이션
+			response.sendRedirect("getBoardList.do");
+			
 		} else if(path.equals("/deleteBoard.do")) {
 			System.out.println("글 삭제 처리");
 		} else if(path.equals("/getBoard.do")) {
@@ -97,6 +117,7 @@ public class DispatcherServlet extends HttpServlet {
 			HttpSession session = request.getSession();
 			session.setAttribute("board", board);
 			response.sendRedirect("getBoard.jsp");
+			
 		} else if(path.equals("/getBoardList.do")) {
 			System.out.println("글 목록 검색 처리");
 			
