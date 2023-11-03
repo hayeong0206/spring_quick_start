dae9495fd0da9c74844de6421a2d4617c8d2d181
lee <lee@localhost>
2023-11-03 09:20:18 +0900

    1.4 클라이언트 요청 처리

--- BoardWeb/src/main/java/com/springbook/view/board/InsertBoardController.java
+++ BoardWeb/src/main/java/com/springbook/view/board/InsertBoardController.java
@@ -1,6 +1,5 @@
 package com.springbook.view.board;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,20 +11,8 @@ import com.springbook.biz.board.impl.BoardDAO;
 public class InsertBoardController {
 	
 	@RequestMapping(value="/insertBoard.do")
-	public void handleRequest(HttpServletRequest request) {
+	public void innsertBoard(BoardVO vo) {
 		System.out.println("글 등록 처리");
-		
-		//1. 사용자 입력 정보 추출
-		//request.setCharacterEncoding("UTF-8");
-		String title = request.getParameter("title");
-		String writer = request.getParameter("writer");
-		String content = request.getParameter("content");
-		
-		//2. DB 연동 처리
-		BoardVO vo = new BoardVO();
-		vo.setTitle(title);
-		vo.setWriter(writer);
-		vo.setContent(content);
 		
 		BoardDAO boardDAO = new BoardDAO();
 		boardDAO.insertBoard(vo);
