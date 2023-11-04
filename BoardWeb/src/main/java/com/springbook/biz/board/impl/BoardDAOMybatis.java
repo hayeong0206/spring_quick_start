7dbcea187d8bd01dad7604e084c73bcd5c87b025
lee <lee@localhost>
2023-11-05 06:32:49 +0900

    3.5 DAO 클래스 구현 방법2 - SqlSessionTemplate 를 bean 등록하여 사용

--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardDAOMybatis.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardDAOMybatis.java
@@ -2,42 +2,39 @@ package com.springbook.biz.board.impl;
 
 import java.util.List;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository; 
 
 import com.springbook.biz.board.BoardVO;
 
 @Repository
-public class BoardDAOMybatis extends SqlSessionDaoSupport {
+public class BoardDAOMybatis {
 	@Autowired
-	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
-		super.setSqlSessionFactory(sqlSessionFactory);
-	}
+	private SqlSessionTemplate mybatis;
 	
 	public void insertBoard(BoardVO vo) {
 		System.out.println("==> Mybatis로 insertBoard() 기능 처리");
-		getSqlSession().insert("BoardDAO.insertBoard", vo);
+		mybatis.insert("BoardDAO.insertBoard", vo);
 	}
 	
 	public void updateBoard(BoardVO vo) {
 		System.out.println("==> Mybatis로 updateBoard() 기능 처리");
-		getSqlSession().update("BoardDAO.insertBoard", vo);
+		mybatis.update("BoardDAO.insertBoard", vo);
 	}
 	
 	public void deleteBoard(BoardVO vo) {
 		System.out.println("==> Mybatis로 deleteBoard() 기능 처리");
-		getSqlSession().delete("BoardDAO.insertBoard", vo);
+		mybatis.delete("BoardDAO.insertBoard", vo);
 	}
 	
 	public BoardVO getBoard(BoardVO vo) {
 		System.out.println("===> Mybatis로 getBoard() 기능 처리");
-		return (BoardVO) getSqlSession().selectOne("BoardDAO.getBoard", vo);
+		return (BoardVO) mybatis.selectOne("BoardDAO.getBoard", vo);
 	}
 	
 	public List<BoardVO> getBoardList(BoardVO vo){
 		System.out.println("===> Mybatis로 getBoardList() 기능 처리");
-		return getSqlSession().selectList("BoardDAO.getBoardList", vo);
+		return mybatis.selectList("BoardDAO.getBoardList", vo);
 	}
 }
