899d2878ec08cab7b6c2b65f6094c39b153df2ce
lee <lee@localhost>
2023-11-06 05:39:05 +0900

    6.6 BoardServiceImpl 클래스 수정 및 테스트

--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardDAOJPA.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardDAOJPA.java
@@ -35,6 +35,6 @@ public class BoardDAOJPA {
 	
 	public List<BoardVO> getBoardList(BoardVO vo){
 		System.out.println("===>JPA로 getBoardList() 기능 처리");
-		return em.createNamedQuery("from BoardVO b order by b.seq desc").getResultList();
+		return em.createQuery("from BoardVO b order by b.seq desc").getResultList();
 	}
 }
